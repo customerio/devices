7c07a1f26e4a034555c9529d7b278197d5643f61
Karn <karn@customer.io>
2022-10-04 18:22:28 +0530

    reduce android data by adding only top manufacturers

--- scripts/android/main.go
+++ scripts/android/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"customerio/devices/scripts/utils"
 	"log"
 	"os"
+	"strings"
+
+	"customerio/devices/scripts/utils"
 )
 
 const (
@@ -50,13 +52,67 @@ func main() {
 
 	log.Println("Fetched", len(data), "devices")
 
+	filteredDevices := filterDevices(data)
+
+	log.Println("Filtered to", len(filteredDevices), "devices based on market share")
+
+	brands := make(map[string]bool)
+	for _, row := range filteredDevices {
+		brands[row[0]] = true
+	}
+
+	log.Println("Found", len(brands), "brands")
+
 	// 3. write csv -> json to the data file
-	if err := utils.WriteJsonToFile(data, AndroidDataFile, "", ""); err != nil {
+	if err := utils.WriteJsonToFile(filteredDevices, AndroidDataFile, "", "  "); err != nil {
 		log.Fatal("Error writing to JSON, check out the *.bak file to restore old contents", err)
 	} else {
-		log.Println("Wrote", len(data), "devices to", AndroidDataFile)
+		log.Println("Wrote", len(filteredDevices), "devices to", AndroidDataFile)
 		if fi, err := os.Stat(AndroidDataFile); err == nil {
-			log.Println("File size:", fi.Size())
+			log.Printf("File size: %d KB", fi.Size()/1024)
 		}
 	}
+}
+
+func filterDevices(data [][]string) [][]string {
+	var filtered [][]string
+	for _, row := range data {
+		if isRetailsBrandingValid(row[0]) {
+			filtered = append(filtered, row)
+		}
+	}
+	return filtered
+}
+
+func isRetailsBrandingValid(retailBranding string) bool {
+	topManufacturers := []string{
+		"samsung",
+		"xiaomi",
+		"oppo",
+		"vivo",
+		"huawei",
+		"realme",
+		"motorola",
+		"amazon",
+		"infinix",
+		"tecno",
+		"lge",
+		"amlogic",
+		"oneplus",
+		"google",
+		"hmd global",
+		"Sony",
+		"rockchip",
+		"zte",
+		"tct (alcatel)",
+		"itel",
+	}
+
+	for _, manufacturer := range topManufacturers {
+		retailBranding = strings.ToLower(retailBranding)
+		if retailBranding == manufacturer {
+			return true
+		}
+	}
+	return false
 }
