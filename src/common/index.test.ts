c166ad28059be236fa6243b1ab124c227d1fab9b
Karn <karn@customer.io>
2022-10-19 13:57:36 +0530

    change getMarketingName api to be universal

--- src/common/index.test.ts
+++ src/common/index.test.ts
@@ -1,29 +1,26 @@
 import { getMarketingName } from './index';
-import { Platform } from '../types';
 
 describe('common', () => {
   test('ios: should get the marketing name', () => {
-    let marketingName = getMarketingName('iPhone14,2', Platform.IOS);
+    let marketingName = getMarketingName('iPhone14,2');
     expect(marketingName).toBe('iPhone 13 Pro');
 
     // case insensitive
-    marketingName = getMarketingName('iphone14,3', Platform.IOS);
+    marketingName = getMarketingName('iphone14,3');
     expect(marketingName).toBe('iPhone 13 Pro Max');
   });
 
   test('android: should get the marketing name', () => {
-    let marketingName = getMarketingName('SM-A300H', Platform.ANDROID);
+    let marketingName = getMarketingName('SM-A300H');
     expect(marketingName).toBe('Galaxy A3');
 
     // case insensitive
-    marketingName = getMarketingName('2201123g', Platform.ANDROID);
+    marketingName = getMarketingName('2201123g');
     expect(marketingName).toBe('Xiaomi 12');
   });
 
-  test('should return the model name if no marketing name is found', () => {
-    let marketingName = getMarketingName('iUNKNOWN', Platform.IOS);
-    expect(marketingName).toBe('iUNKNOWN');
-    marketingName = getMarketingName('SM-UNKNOWN', Platform.ANDROID);
-    expect(marketingName).toBe('SM-UNKNOWN');
+  test('should return null if no marketing name is found', () => {
+    const marketingName = getMarketingName('iUNKNOWN');
+    expect(marketingName).toBeNull();
   });
 });
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,25 +1,23 @@
 import { getIosDevices } from '../ios';
 import { getAndroidDevices } from '../android';
-import { Platform } from '../types';
 
 /**
  * Get marketing name for a given device model or identifier.
- * If no device is found, returns the model or identifier.
+ * If no device is found, returns null
  *
  * @param deviceModel - case insensitive
- * @param os
  */
-export const getMarketingName = (deviceModel: string, os: Platform): string => {
-  if (os === Platform.IOS) {
-    const devices = getIosDevices(deviceModel);
-    if (devices.length > 0) {
-      return devices[0].generation;
-    }
-  } else {
-    const devices = getAndroidDevices(deviceModel);
-    if (devices.length > 0) {
-      return devices[0].marketingName;
-    }
+export const getMarketingName = (deviceModel: string): string | null => {
+  const iosDevices = getIosDevices(deviceModel);
+
+  if (iosDevices.length > 0) {
+    return iosDevices[0].generation;
   }
-  return deviceModel;
+
+  const androidDevices = getAndroidDevices(deviceModel);
+  if (androidDevices.length > 0) {
+    return androidDevices[0].marketingName;
+  }
+
+  return null;
 };
